33881b2f19ee20c30fd0bcdf89e478970139e265
IshanKatkar <73028425+IshanKatkar@users.noreply.github.com>
2021-02-17 13:32:49 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,14 +5,17 @@ const Constraint = Matter.Constraint;
 
 function preload(){
 
-    umbrellaImg = loadImage("images/walking_1.png")
-    
+  
+    manImg=loadImage("images/Walking Frame/walking_1.png")
 }
 
 function setup(){
-    createCanvas(1600,1400,30,30)
+    createCanvas(1600,1400,30,30);
+    engine = Engine.create();
+    world = engine.world;
     
     umbrella = new Umbrella(200,200,20,20);
+    umbrella.addImage(manImg);
     
 }
 
